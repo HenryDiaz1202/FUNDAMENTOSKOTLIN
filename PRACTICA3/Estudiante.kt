96d1cc152d4b0d6e4a4ef8aadc5de830cbe0ddae
HenryDiaz1202 <henrydiaz1202@gmail.com>
2020-11-26 16:31:36 -0600

    Polimorfismo listo

---
+++ PRACTICA3/Estudiante.kt
@@ -0,0 +1,13 @@
+open class Persona(var nombre:String, var apellido:String)
+
+class Estudiante(nombre:String, apellido:String,
+var asignatura:String,
+ var profesor:String):
+ Persona(nombre,apellido)
+
+fun main(){
+
+    var estudiante = Estudiante("James","Reyes","Recursos Naturales","Eddy Herrera")
+
+    println("Asignatura: ${estudiante.asignatura} Estudiante: ${estudiante.nombre}")
+}
