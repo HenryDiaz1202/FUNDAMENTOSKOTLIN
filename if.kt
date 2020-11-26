00f35d70260c262d21adedc129586beb5fe99654
HenryDiaz1202 <henrydiaz1202@gmail.com>
2020-11-26 12:50:33 -0600

    comparación de edad

---
+++ if.kt
@@ -0,0 +1,13 @@
+fun main(){
+   /* val fecha = Date()
+    val hoy = if (fecha.year == 2019) true else false
+    println("$hoy");*/
+
+    val edad = 20
+    if(edad >= 20)
+    {
+    println("eres mayor de edad")
+    } else {
+    println("aun eres menor")
+    }
+}
