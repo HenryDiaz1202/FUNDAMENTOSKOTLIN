2dec2658b8fde40cae9d29b31a992dff3e39255f
HenryDiaz1202 <henrydiaz1202@gmail.com>
2020-11-26 18:02:33 -0600

    Ejercicio de los ciclos completo

--- PRACTICA2/Ciclos.kt
+++ PRACTICA2/Ciclos.kt
@@ -1,6 +1,24 @@
 fun main(){
-    while (true) {
-        println("Esto se imprimirá por un largo tiempo!")
-       }
-       
+
+    //WHILE
+
+        //infinito
+        /*while (true) {
+
+            println("Esto se imprimirá por un largo tiempo!")
+        }  */  
+        var a:Int = 0
+        while (a<=10) {
+
+            println("Esto se imprimirá por un largo tiempo!")
+            a++;
+        }  
+    //FOR
+        val array = arrayOf("a", "b", "c")
+        for (i in array.indices) {
+        println(array[i])
+        }
+        for (i in 6 downTo 0 step 2) {
+        println(i)
+        }
 }
